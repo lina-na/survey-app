0aa3060b248ca503b7cc08385d9ac910927db9b7
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2016-04-23 22:18:39 -0700

    Update config.json

--- migrations/20160418202926-unnamed-migration.js
+++ migrations/20160418202926-unnamed-migration.js
@@ -2,13 +2,7 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     return queryInterface.createTable('guests', {
       id: {
         type: Sequelize.INTEGER,
@@ -24,13 +18,7 @@ module.exports = {
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.dropTable('users');
-    */
     return queryInterface.dropTable('guests');
   }
 };
